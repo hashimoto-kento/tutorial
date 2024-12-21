3a689754f3a1c3413e310ae40f42b8c7c0336604
DESKTOP-J40HMMS\Owner <bug.bee.ron@gmail.com>
2024-12-21 11:20:03 +0900

    demo application

--- random-cat/pages/index.tsx
+++ random-cat/pages/index.tsx
@@ -1,15 +1,55 @@
-import { NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
+import { useState } from "react";
+import styles from "./index.module.css";
 
-const IndexPage: NextPage = () => {
-  return <div>猫画像予定地</div>;
+type Props = {
+  initialImageUrl: string;
+};
+
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
+  const [imageUrl, setImageUrl] = useState(initialImageUrl);
+  const [loading, setLoading] = useState(false);
+  // クライアントサイドで初期画像を取得する場合に使用
+  // useEffect(() => {
+  //   fetchImage().then((newImage) => {
+  //     setImageUrl(newImage.url);
+  //     setLoading(false);
+  //   });
+  // }, []);
+  const handleClick = async () => {
+    setLoading(true);
+    const newImage = await fetchImage();
+    setImageUrl(newImage.url);
+    setLoading(false);
+  };
+  return (
+    <div className={styles.page}>
+      <button onClick={handleClick} className={styles.button}>
+        他の猫も見てみる
+      </button>
+      <div className={styles.frame}>
+        {loading || <img src={imageUrl} className={styles.img} />}
+      </div>
+    </div>
+  );
 };
 export default IndexPage;
 
-const fetchImage = async () => {
-  const res = await fetch('https://api.thecatapi.com/v1/images/search');
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const image = await fetchImage();
+  return {
+    props: {
+      initialImageUrl: image.url,
+    },
+  };
+};
+
+type Image = {
+  url: string;
+};
+const fetchImage = async (): Promise<Image> => {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
   const images = await res.json();
   console.log(images);
   return images[0];
 };
-
-// fetchImage();
